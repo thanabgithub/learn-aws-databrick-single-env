ab86aa096daa1550fa35bf03a6095f3fb8bf1902
root <root@rog-ally-x>
2025-06-21 16:13:03 +0900

    fix syntax based on official example

--- modules/s3-vpc/main.tf
+++ modules/s3-vpc/main.tf
@@ -223,25 +223,25 @@ resource "aws_s3_bucket_versioning" "this" {
 
 # Bucket encryption (Enhanced)
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  count = local.create_bucket ? 1 : 0
+  count = local.create_bucket && length(keys(var.server_side_encryption_configuration)) > 0 ? 1 : 0
 
   bucket                = aws_s3_bucket.this[0].id
   expected_bucket_owner = var.expected_bucket_owner
 
   dynamic "rule" {
-    for_each = length(keys(var.server_side_encryption_configuration)) > 0 ? 
-      try(flatten([var.server_side_encryption_configuration["rule"]]), []) : 
-      [{}]
+    for_each = try(flatten([var.server_side_encryption_configuration["rule"]]), [])
 
     content {
       bucket_key_enabled = try(rule.value.bucket_key_enabled, var.bucket_key_enabled, null)
 
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = try(rule.value.apply_server_side_encryption_by_default.sse_algorithm, local.final_encryption_type)
-        kms_master_key_id = try(
-          rule.value.apply_server_side_encryption_by_default.kms_master_key_id,
-          local.final_encryption_type == "aws:kms" ? var.kms_key_id : null
-        )
+      dynamic "apply_server_side_encryption_by_default" {
+        for_each = try([rule.value.apply_server_side_encryption_by_default], [])
+
+        content {
+          sse_algorithm     = apply_server_side_encryption_by_default.value.sse_algorithm
+          kms_master_key_id = try(apply_server_side_encryption_by_default.value.kms_master_key_id, 
+            apply_server_side_encryption_by_default.value.sse_algorithm == "aws:kms" ? var.kms_key_id : null)
+        }
       }
     }
   }
